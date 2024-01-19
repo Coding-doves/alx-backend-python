b6ba7723fddab05ca1335a76d68d5b605540529a
Ada <106437177+Coding-doves@users.noreply.github.com>
2024-01-19 19:30:41 +0100

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,6 +4,8 @@ Test file for printing the correct output of the wait_n coroutine
 '''
 import asyncio
 import random
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
