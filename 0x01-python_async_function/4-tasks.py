0af29951416d14092da15c0ff6ae0c5c5fae0e63
Ada <106437177+Coding-doves@users.noreply.github.com>
2024-01-19 23:31:55 +0100

    Update 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
 ''' async '''
+import asyncio
+from typing import List
+task_wait_random = __import__("3-tasks").task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    ''' spawn using asyncio.gather() with task_wait_random '''
+    tasks = [task_wait_random(max_delay) for i in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return delays
