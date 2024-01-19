1347e5a44daeca3b7651a519bee5c1a9b0243c9e
Ada <106437177+Coding-doves@users.noreply.github.com>
2024-01-19 23:57:06 +0100

    Update 2-measure_runtime.py

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -9,12 +9,12 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 def measure_time(n: int, max_delay: int) -> float:
     ''' time it takes to spawn each '''
     start_time = time.time()
-    
+
     ''' Run the wait_n coroutine and get the result '''
     asyncio.run(wait_n(n, max_delay))
-    
+
     end_time = time.time()
     total_time = end_time - start_time
-    
+
     ''' Return the average time per operation '''
     return total_time / n
