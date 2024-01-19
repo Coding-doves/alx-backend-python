a84a145faca80e4086748bc67e5e08d1df1c7b58
Ada <106437177+Coding-doves@users.noreply.github.com>
2024-01-19 20:09:17 +0100

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     ''' spawn using asyncio.gather() '''
     tasks = [wait_random(max_delay) for i in range(n)]
     delays = await asyncio.gather(*tasks)
