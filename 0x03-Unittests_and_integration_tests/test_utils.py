9aecdb206fb7d4a51af000d6b7755624f17aa870
dove <obenedicta4@gmail.com>
2024-02-05 13:04:12 +0100

    task0

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+''' unittest '''
+import unittest
+from utils import access_nested_map
+from parameterized import parameterized
+from typing import Mapping, Tuple, Union
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    ''' parameter '''
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+
+    def test_access_nested_map(self, nested_map: Mapping, path: Tuple[str], expected: Union[Mapping, int]) -> None:
+        ''' test '''
+        self.assertEqual(access_nested_map,(nested_map), path, expected)
