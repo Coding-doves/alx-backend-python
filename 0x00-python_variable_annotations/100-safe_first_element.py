94ade30edcab0fd7b6e2d8f217e882726b67c31c
Ada <106437177+Coding-doves@users.noreply.github.com>
2024-02-24 14:06:53 +0100

    Update 100-safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Duck typing - first element of a sequence"""
-from typing import Sequence, Any, Union, Optional
+
+from typing import Any, List, Union
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-     '''The types of the elements of the input are not know'''
+def safe_first_element(lst: List[Any]) -> Union[Any, None]:
     if lst:
         return lst[0]
     else:
